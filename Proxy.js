8b804e1aca597f5e2167375a466e89b9e32d8185
Raja Saini <rajasaini22086@gmail.com>
2026-06-26 14:35:22 +0530

    Update Proxy.js

--- Proxy.js
+++ Proxy.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
 
+app.use('/', (req, res) => {
+  req.pipe(
+    require('http').request({
+      hostname: 'localhost',
+      port: 3002,
+      path: '/api/client' + req.url,
+      method: req.method,
+      headers: req.headers
+    }, (r) => r.pipe(res))
+  );
+});
+
+app.listen(80);

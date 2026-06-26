42749188d59841e651c8ed5cd61ce0239c568742
Raja Saini <rajasaini22086@gmail.com>
2026-06-26 15:07:59 +0530

    Update ondc.routes.ts

--- logistics-app-api/app/routes/ondc.routes.ts
+++ logistics-app-api/app/routes/ondc.routes.ts
@@ -4,22 +4,15 @@ import OndcController from '../controllers/ondc.controller'
 const router = express.Router()
 const ondcController = new OndcController()
 
-router.post('/client/search', ondcController.agentSearch)
-
-router.post('/client/Init', ondcController.orderInit)
-
-router.post('/client/confirm', ondcController.orderConfirm)
-
-router.post('/client/update', ondcController.orderUpdate)
-
-router.post('/client/status', ondcController.orderStatus)
-
-router.post('/client/support', ondcController.support)
-
-router.post('/client/cancel', ondcController.cancel)
-router.post('/client/track', ondcController.track)
-
-router.post('/client/issue', ondcController.issue)
-router.post('/client/issue_status', ondcController.issueStatus)
+router.post('/search', ondcController.agentSearch)
+router.post('/init', ondcController.orderInit)
+router.post('/confirm', ondcController.orderConfirm)
+router.post('/update', ondcController.orderUpdate)
+router.post('/status', ondcController.orderStatus)
+router.post('/support', ondcController.support)
+router.post('/cancel', ondcController.cancel)
+router.post('/track', ondcController.track)
+router.post('/issue', ondcController.issue)
+router.post('/issue_status', ondcController.issueStatus)
 
 export default router
